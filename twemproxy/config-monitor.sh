7122339fefd798049b8e42ef9bf380a0ad122c05
Trevor Porter <trkporter@ucdavis.edu>
2017-08-11 14:19:22 -0700

    Add python args

--- twemproxy/config-monitor.sh
+++ twemproxy/config-monitor.sh
@@ -6,21 +6,27 @@ PERIOD=${PERIOD:-10}
 YAML=${YAML:-/twemproxy.yaml}
 PID_FILE=${PID_FILE:-/twemproxy.pid}
 CONFIGURATOR=${CONFIGURATOR:-/create-config.py}
+TEMP_CONFIG=/tmp/twemproxy.yaml.update
+EMPTY=/empty_file
 
-apk add --update curl;
+echo "" > $EMPTY
 
 while true; do
   sleep $PERIOD;
-  python $CONFIGURATOR > /tmp/twemproxy.yaml.update;
+  python $CONFIGURATOR > $TEMP_CONFIG;
 
-  cmp /tmp/twemproxy.yaml.update $YAML;
+  cmp $TEMP_CONFIG $EMPTY
   if [ $? -ne '0' ]; then
-      mv /tmp/twemproxy.yaml.update $YAML;
-      if [ -f $PID_FILE ]; then
-        PID=`cat $PID_FILE`
-        rm -f $PID_FILE;
-        kill -s 15 $PID;
-      fi
+    cmp $TEMP_CONFIG $YAML;
+    if [ $? -ne '0' ]; then
+        mv /tmp/twemproxy.yaml.update $YAML;
+        if [ -f $PID_FILE ]; then
+          PID=`cat $PID_FILE`
+          rm -f $PID_FILE;
+          kill -s 15 $PID;
+        fi
+    fi
+
   fi
 
 done
--- twemproxy/create-config.py
+++ twemproxy/create-config.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import argparse
+import json
+import logging
 import os
 import requests
 import types
@@ -10,12 +12,13 @@ DEFAULT_CRT_PATH = '/var/run/secrets/kubernetes.io/serviceaccount/ca.crt'
 DEFAULT_TOKEN_PATH = '/var/run/secrets/kubernetes.io/serviceaccount/token'
 DEFAULT_TEMPLATE_PATH = '/twemproxy.template'
 DEFAULT_REDIS_PORT = 6379
-DEFAULT_LABEL_SELECTOR = {
-    'name': 'redis-node'
-}
+DEFAULT_LABEL_SELECTOR = '{"name": "redis-node"}'
 
 def create_config(args):
-    pod_ips = all_pod_ips()
+    pod_ips = all_pod_ips(args)
+    if not pod_ips:
+        return ''
+
     with open(DEFAULT_TEMPLATE_PATH, 'r') as template:
         config = template.read().rstrip() + '\n'
         config += server_config(pod_ips)
@@ -27,8 +30,17 @@ def server_config(pod_ips):
         servers += '    - {}:{}:1\n'.format(ip, DEFAULT_REDIS_PORT)
     return servers
 
-def all_pod_ips():
-    all_pods = all_kubernetes_pods()
+def all_pod_ips(args):
+    all_pods = api_get_redis_pods(
+        args.service_url,
+        args.api_version,
+        args.api_token_path,
+        args.api_crt_path,
+        args.label_selectors
+    )
+    if not all_pods or len(all_pods) == 0:
+        return None
+
     ip_list = []
     for pod in all_pods:
         ip = get_nested(pod, 'status.podIP')
@@ -37,17 +49,13 @@ def all_pod_ips():
 
     return ip_list
 
-def all_kubernetes_pods():
-    all_pods = api_get_redis_pods()
-    return all_pods.get('items')
-
-def api_get_redis_pods():
-    api_base_url = api_service_url()
-    pod_api_url = urlparse.urljoin(api_base_url, 'namespaces/default/pods')
-    api_auth_header = api_authorization_header()
-    label_selector = api_label_selector(DEFAULT_LABEL_SELECTOR)
-    pod_api_response = requests.get(pod_api_url, headers=api_auth_header, params=label_selector, verify=DEFAULT_CRT_PATH)
-    return pod_api_response.json()
+def api_get_redis_pods(service_url, api_version, token_path, crt_path, label_selectors):
+    pod_api_url = urlparse.urljoin(service_url + api_path(api_version), 'namespaces/default/pods')
+    auth_header = api_authorization_header(token_path)
+    label_selector = api_label_selector(label_selectors)
+    pod_api_response = requests.get(pod_api_url, headers=auth_header, params=label_selector, verify=crt_path)
+    api_json = pod_api_response.json()
+    return api_json.get('items')
 
 def api_label_selector(label_criteria):
     if not label_criteria or len(label_criteria) <= 0:
@@ -62,12 +70,15 @@ def api_label_selector(label_criteria):
         'labelSelector': criteria_str
     }
 
-def api_service_url():
-    base_url = 'https://kubernetes:{}/api/v1/'.format(os.environ['KUBERNETES_PORT_443_TCP_PORT'])
+def default_api_service_url():
+    base_url = 'https://kubernetes:{}'.format(os.environ['KUBERNETES_PORT_443_TCP_PORT'])
     return base_url
 
-def api_authorization_header():
-    token = open(DEFAULT_TOKEN_PATH).read()
+def api_path(api_version):
+    return '/api/{}/'.format(api_version)
+
+def api_authorization_header(token_path):
+    token = open(token_path).read()
     auth_value = 'Bearer {}'.format(token)
     return {
         'Authorization': auth_value
@@ -97,29 +108,40 @@ def main(args):
     print create_config(args)
 
 def process_args():
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('--kubernetes-service',
-    #                     default=_default_kubernetes_service_url(),
-    #                     help='Kubernetes service to poll')
-    # parser.add_argument('--api-version',
-    #                     default='v1beta3',
-    #                     help='Kubernetes API version')
-    # parser.add_argument('--name-label',
-    #                     default='redis-node',
-    #                     help='Service name label to poll for')
-    # parser.add_argument('--config-template',
-    #                     default=(os.environ.get('TEMPLATE') or
-    #                              '/twemproxy.template'),
-    #                     help='twemproxy config template')
-    # parser.add_argument('--debug',
-    #                     action='store_true',
-    #                     default=False,
-    #                     help='Display debug messages')
-    # args = parser.parse_args()
-    # if args.debug:
-    #   logging.getLogger('root').setLevel(logging.DEBUG)
-    return None
-    # return args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--service-url',
+                        default=default_api_service_url(),
+                        help='Kubernetes service to poll')
+    parser.add_argument('--api-version',
+                        default='v1',
+                        help='Kubernetes API version')
+    # Auto parses json into dictionary
+    parser.add_argument('--label-selectors',
+                        default=DEFAULT_LABEL_SELECTOR,
+                        type=json.loads,
+                        help='JSON label selectors to identify redis pods by')
+    parser.add_argument('--config-template',
+                        default=(os.environ.get('TEMPLATE') or DEFAULT_TEMPLATE_PATH),
+                        help='Twemproxy config template')
+    parser.add_argument('--redis-port',
+                        default=DEFAULT_REDIS_PORT,
+                        type=int,
+                        help='Port of redis pods')
+    parser.add_argument('--api-token-path',
+                        default=DEFAULT_TOKEN_PATH,
+                        help='Path to the Kubernetes API token')
+    parser.add_argument('--api-crt-path',
+                        default=DEFAULT_CRT_PATH,
+                        help='Path to the Kubernetes API crt')
+    parser.add_argument('--debug',
+                        action='store_true',
+                        default=False,
+                        help='Display debug messages')
+    args = parser.parse_args()
+    if args.debug:
+      logging.getLogger('root').setLevel(logging.DEBUG)
+
+    return args
 
 
 if __name__ == '__main__':
