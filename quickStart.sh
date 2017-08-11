cdc504c49c2e5546bc3f85c5b3000af4de054d10
Trevor Porter <trkporter@ucdavis.edu>
2017-08-11 11:56:27 -0700

    Update readme, documentation

--- quickStart.sh
+++ quickStart.sh
@@ -1,5 +1,7 @@
+# Start the redis-node pods
 kubectl create -f deployments/redis-node.yaml;
 kubectl create -f services/redis-node.yaml;
 
+# And start the twemproxy pods
 kubectl create -f deployments/twemproxy.yaml;
 kubectl create -f services/twemproxy.yaml;
